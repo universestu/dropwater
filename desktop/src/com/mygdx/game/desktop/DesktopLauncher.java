3c7baf3e9c32fcefccef3f18e2b3557a0d5ed2e3
universestu <universestu@gmail.com>
2016-11-05 00:22:21 +0700

    Initial commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Drop";
+		cfg.width = 800;
+		cfg.height = 480;
+		new LwjglApplication(new MyGdxGame(), cfg);
+
+	}
+}
